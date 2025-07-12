75cda5d9db2746685700a5434b2f596e4eb9d65f
Manh Dao <manhdao1006@gmail.com>
2025-07-12 19:17:15 +0700

    fix-db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,7 @@ INSERT INTO dichvu (ten_dich_vu)
 VALUES 
     ('Đất'),
     ('Nhà và Đất');
+
+INSERT INTO khachhang (email, so_dien_thoai, ten_khach_hang)
+VALUES
+    ('vanhoang@example.com', '1234567890', N'Nguyễn Văn Hoàng')
