df11f9bc083b285ca321a47a90999601997ce79b
Manh Dao <manhdao1006@gmail.com>
2025-07-12 18:28:41 +0700

    fix

--- src/main/java/com/tech/fstest/controller/HealthController.java
+++ src/main/java/com/tech/fstest/controller/HealthController.java
@@ -1,11 +1,13 @@
 package com.tech.fstest.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class HealthController {
 
     @GetMapping("/health")
